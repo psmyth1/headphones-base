5c49402bddc59cc3b8b0ca2c381b256fc3f31408
psmyth1 <psmyth1@alumni.nd.edu>
2014-01-22 07:01:13 -0500

    test

--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,2 @@
-set APP_ROOT=%WWW_ROOT%\headphones\dev
+set APP_ROOT=%WWW_ROOT%\headphones\master
 python.exe %APP_ROOT%\bin\Headphones.py --datadir="%APP_ROOT%\conf" --config="%APP_ROOT%\conf\config.ini"
